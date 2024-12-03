d746934fe157899bb531a8b0363d60607249e8cf
Kyle Esterhuizen <kyle.esterhuizen@directtransact.co.za>
2024-12-03 14:17:04 +0200

    updates

--- lvim/config.lua
+++ lvim/config.lua
@@ -2,9 +2,19 @@ vim.opt.relativenumber = true
 vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 2 -- insert 2 spaces for a tab
 vim.opt.relativenumber = true -- relative line numbers
-vim.opt.wrap = true -- wrap lines
+-- lvim.transparent_window = true
+-- vim.opt.wrap = true -- wrap lines
 -- Read the docs: https://www.lunarvim.org/docs/configuration
 -- Example configs: https://github.com/LunarVim/starter.lvim
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.plugins = {
+  "scottmckendry/cyberdream.nvim",
+  "iagorrr/noctishc.nvim",
+  "kartikp10/noctis.nvim",
+  "rktjmp/lush.nvim"
+}
+
+lvim.colorscheme = "cyberdream"
